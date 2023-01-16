4556b035712a26b3b6d65f8f9d53279cdbc17eb6
h.young <cocotree221@gmail.com>
2023-01-16 15:33:10 +0900

    최종 빌드

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 //express 서버
 const express = require("express");
 const { dirname } = require("path");
+
 //서버 경로 모듈
 const path = require("path");
 
 //mongoose 모듈
 const mongoose = require("mongoose");
-const { Todo } = require("./model/TodoModel.js");
+// const { Todo } = require("./model/TodoModel.js");
+
+var cors = require("cors");
 
 //개발 인증관련
 const config = require("./config/key.js");
 
 //express 인스턴스 생성
 const app = express();
+
 //요청이 들어오면 json 사용 및 url 인코딩 진행해줌
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Post 관련 Router 연결
+app.use("/api/post", require("./router/Post.js"));
+//User 관련 Router 연결
+app.use("/api/user", require("./router/User.js"));
+
 //포트번호
 const port = 5000;
+
+let corsOptions = {
+  origin: "*", // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+};
+app.use(cors(corsOptions));
 //고정(static)된 path 경로를 설정한다.
-app.use(express.static(path.join(__dirname, "../client/build/")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 //서버가 요청을 받아들이기 위해서 대기 중
 app.listen(port, () => {
@@ -35,47 +51,113 @@ app.listen(port, () => {
     });
 });
 
-//요청:Rquest
+//요청:Request
 //응답:Response
 app.get("/", (req, res) => {
   //파일을 보여줌
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 //주소가 없는 경우에 강제 url 이동
 app.get("*", (req, res) => {
   res.send("(❤´艸｀❤) 오타 났나봐용 여기아니");
 });
 
-//할일 등록
-app.post("/api/post/submit", (req, res) => {
-  // console.log(req.body);
-  let temp = req.body;
-  const todoPost = new Todo(temp);
-  todoPost
-    .save()
-    .then(() => {
-      //데이터 저장이 성공한 경우
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      //데이터 저장이 실패한 경우
-      console.log(err);
-      res.status(400).json({ success: false });
-    });
-  //200단위는 서버가 살아있음. 400단위는 없는파일, 없는api. 500단위는 서버error
-});
+// //할일 등록 (router/Post.js 로 옯겨줌)
+// app.post("/api/post/submit", (req, res) => {
+//   // console.log(req.body);
+//   let temp = req.body;
+//   const todoPost = new Todo(temp);
+//   todoPost
+//     .save()
+//     .then(() => {
+//       //데이터 저장이 성공한 경우
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       //데이터 저장이 실패한 경우
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+//   //200단위는 서버가 살아있음. 400단위는 없는파일, 없는api. 500단위는 서버error
+// });
 
-//목록 읽어오기
-app.post("/api/post/list", (req, res) => {
-  console.log("전체목록 호출");
-  Todo.find({})
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      res.status(200).json({ success: true, initTodo: doc });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({ success: false });
-    });
-});
+// //목록 읽어오기
+// app.post("/api/post/list", (req, res) => {
+//   console.log("전체목록 호출");
+//   Todo.find({})
+//     .exec()
+//     .then((doc) => {
+//       console.log(doc);
+//       res.status(200).json({ success: true, initTodo: doc });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.status(400).json({ success: false });
+//     });
+// });
+
+// //할 일의 complted를 업데이트
+// app.post("/api/post/updatetoggle", (req, res) => {
+//   // console.log(req.body)
+//   let temp = {
+//     completed: req.body.completed,
+//   };
+//   //mongoose 문서참조
+//   Todo.updateOne({ id: req.body.id }, { $set: temp })
+//     .exec()
+//     .then(() => {
+//       // console.log("completed 업데이트 완료");
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// //타이틀 업데이트
+// app.post("/api/post/updatetitle", (req, res) => {
+//   // console.log(req.body)
+//   let temp = {
+//     title: req.body.title,
+//   };
+//   //mongoose 문서참조
+//   Todo.updateOne({ id: req.body.id }, { $set: temp })
+//     .exec()
+//     .then(() => {
+//       // console.log("completed 업데이트 완료");
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
+
+// //할일 삭제
+// app.post("/api/post/delete", (req, res) => {
+//   console.log(req.body);
+//   Todo.deleteOne({ id: req.body.id })
+//     .exec()
+//     .then(() => {
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
+
+// //할일 전체 삭제
+// app.post("/api/post/deleteAll", (req, res) => {
+//   Todo.deleteMany()
+//     .exec()
+//     .then(() => {
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json({ success: false });
+//     });
+// });
+
+app.post("/api/user/register", (req, res) => {});
